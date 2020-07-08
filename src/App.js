23b2803afe912e03b2b7d1384cec646a6cce86ae
Melissa Astbury <melissajastbury@gmail.com>
2020-07-08 13:38:52 +0200

    added in the required styling to my index.scss folder

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.scss";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <div className="app-wrapper">
+        <h1>Hello World</h1>
+      </div>
     </div>
   );
 };
