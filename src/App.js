85b2b7caf7dfe98b209c5e31c3b3cf1f3a612bf0
Melissa Astbury <melissajastbury@gmail.com>
2020-07-09 11:07:30 +0200

    the two transaction history components have been made and rendered for both income and expenses

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Balance from "./components/Balance/Balance";
 import AddTransaction from "./components/AddTransaction/AddTransaction";
+import IncomeList from "./components/IncomeList/IncomeList";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
         <Header />
         <Balance />
         <AddTransaction />
+        <IncomeList />
+        <ExpenseList />
       </div>
     </div>
   );
---
+++ src/components/IncomeList/IncomeList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const IncomeList = () => {
+  return (
+    <div className="transactions transactions-income">
+      <h2>Transaction History</h2>
+      <ul className="transaction-list">
+        <li className="transaction">
+          <span className="transaction-text">Salaray</span>
+          <span className="transaction-amount">£3000</span>
+          <button className="delete-btn">
+            <i className="fas fa-trash"></i>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default IncomeList;
