3756381e7e53793cd9d4b0ab716434e10047424f
Melissa Astbury <melissajastbury@gmail.com>
2020-07-10 10:43:03 +0200

    created an onChnage for the incomeText and incomeAmount. Also made sure that the incomeAmount does not override the text you have inputted

--- src/components/AddTransaction/AddTransaction.js
+++ src/components/AddTransaction/AddTransaction.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+  const [income, setIncome] = useState({
+    incomeText: "",
+    incomeAmount: 0,
+  });
+
+  const onChangeIncome = (e) => {
+    //this will access the exact property value
+    // "...income" - this keeps the text and the number instead of the number over writting the text
+    setIncome({ ...income, [e.target.name]: e.target.value });
+    console.log(income);
+  };
+
   return (
     <div className="form-wrapper">
       <form>
         <div className="input-group income">
-          <input type="text" placeholder="Add Income..." autoComplete="off" />
-          <input type="number" placeholder="Amount" autoComplete="off" />
+          <input
+            type="text"
+            name="incomeText"
+            placeholder="Add Income..."
+            autoComplete="off"
+            onChange={onChangeIncome}
+          />
+          <input
+            type="number"
+            name="incomeAmount"
+            placeholder="Amount"
+            autoComplete="off"
+            onChange={onChangeIncome}
+          />
           <input type="submit" value="submit" />
         </div>
       </form>
