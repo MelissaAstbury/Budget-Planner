eb416a8af04c1dbdb782a3dc3823d4b87a960977
Melissa Astbury <melissajastbury@gmail.com>
2020-07-10 10:11:37 +0200

    mapped through each 'income transaction' from the GlobalState so each render as an individual income

--- src/components/IncomeList/IncomeList.js
+++ src/components/IncomeList/IncomeList.js
@@ -2,17 +2,26 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 const IncomeList = () => {
+  const { incomeTransactions } = useContext(GlobalContext);
+  console.log(incomeTransactions);
+
   return (
     <div className="transactions transactions-income">
       <h2>Transaction History</h2>
       <ul className="transaction-list">
-        <li className="transaction">
-          <span className="transaction-text">Salaray</span>
-          <span className="transaction-amount">£3000</span>
-          <button className="delete-btn">
-            <i className="fas fa-trash"></i>
-          </button>
-        </li>
+        {incomeTransactions.map((incomeTransaction) => (
+          <li className="transaction">
+            <span className="transaction-text">
+              {incomeTransaction.incomeText}
+            </span>
+            <span className="transaction-amount">
+              {incomeTransaction.incomeAmount}
+            </span>
+            <button className="delete-btn">
+              <i className="fas fa-trash"></i>
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
