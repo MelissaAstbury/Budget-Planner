9a3a5bd03146b230164555f70c0dac2436be44d1
Melissa Astbury <melissajastbury@gmail.com>
2020-07-14 12:12:35 +0200

    each transaction list is now added up automatically and showing in the correct column

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Balance = () => {
+  const { incomeTransactions, expenseTransactions } = useContext(GlobalContext);
+
+  const incomeAmounts = incomeTransactions.map(
+    (incomeTransaction) => incomeTransaction.incomeAmount
+  );
+
+  const expenseAmounts = expenseTransactions.map(
+    (expenseTransaction) => expenseTransaction.expenseAmount
+  );
+
+  const totalIncome = incomeAmounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const totalExpense = expenseAmounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
   return (
     <div className="balance">
       <h2>Your Balance:</h2>
@@ -8,11 +27,11 @@ const Balance = () => {
       <div className="income-expense">
         <div className="plus">
           <h3>Income</h3>
-          <p>+£0.00</p>
+          <p>+{totalIncome}</p>
         </div>
         <div className="minus">
           <h3>Expenses</h3>
-          <p>-£0.00</p>
+          <p>-£{totalExpense}</p>
         </div>
       </div>
     </div>
