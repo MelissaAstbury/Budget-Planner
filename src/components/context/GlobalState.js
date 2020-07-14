0de10b3c0bbd0eed1978f5b1da8fe1ea53d7f4e4
Melissa Astbury <melissajastbury@gmail.com>
2020-07-14 12:32:10 +0200

    when the page is refreshed the local storage is now kept so no income or expenses are removed

--- src/components/context/GlobalState.js
+++ src/components/context/GlobalState.js
@@ -1,23 +1,28 @@
-import React, { useContext, useReducer, createContext } from "react";
+import React, { useReducer, createContext, useEffect } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-  incomeTransactions: [
-    { id: 1, incomeText: "Freelance", incomeAmount: 1300 },
-    { id: 2, incomeText: "Salary", incomeAmount: 2600 },
-    { id: 3, incomeText: "Baby Sitting", incomeAmount: 500 },
-  ],
-  expenseTransactions: [
-    { id: 4, expenseText: "Rent", expenseAmount: 650 },
-    { id: 5, expenseText: "Phone", expenseAmount: 50 },
-    { id: 6, expenseText: "Food Shopping", expenseAmount: 350 },
-  ],
+  incomeTransactions:
+    JSON.parse(localStorage.getItem("incomeTransactions")) || [],
+  expenseTransactions:
+    JSON.parse(localStorage.getItem("expenseTransactions")) || [],
 };
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "incomeTransactions",
+      JSON.stringify(state.incomeTransactions)
+    );
+    localStorage.setItem(
+      "expenseTransactions",
+      JSON.stringify(state.expenseTransactions)
+    );
+  });
 
   const addIncome = (incomeTransaction) => {
     dispatch({
